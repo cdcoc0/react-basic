f6277f11298ad2d6ce4725c3fb5c7d1f8a9c16f4
velykitty <jiyunee13@gmail.com>
2020-12-25 01:33:05 +0900

    state tutorial

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
+  //render보다 먼저 실행
+  //component 초기화하는 함수
+  constructor(props) {
+    super(props);
+    this.state = {
+      Subject: {title: "WEB", sub: "world wide webb!"}
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide webb!"></Subject>
+        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is a Hypertext Markup Language"></Content>
       </div>
