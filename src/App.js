9651d49f94cbd6097cf77c6cae442e6e6bdce895
velykitty <jiyunee13@gmail.com>
2020-12-29 13:00:17 +0900

    component event

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: "read",
+      selected_content_id: 2,
       subject: {title: "WEB", sub: "world wide webb!"},
       welcome: {title: "welcome", desc: "Hey there!"},
       contents: [
@@ -29,15 +30,25 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode ==="read") {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i += 1;
+      }
     }
     //console.log("render", this)
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}
         onChangePage={function() {
-          this.setState({mode: "welcome"})
+          this.setState({
+            mode: "welcome"
+          });
         }.bind(this)}></Subject>
         {/* <header>
               <h1><a href="/" onClick={function(e) {
@@ -53,8 +64,10 @@ class App extends Component {
               {this.state.subject.sub}
           </header> */}
         <TOC data={this.state.contents} 
-        onChangePage={function() {
-          this.setState({mode: "read"})
+        onChangePage={function(id) {
+          this.setState({
+            mode: "read",
+            selected_content_id: Number(id)})
         }.bind(this)}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -10,9 +10,11 @@ class TOC extends Component {
         lists.push(
         <li key={data[i].id}>
           <a href={"/content/"+data[i].id}
+          data-id={data[i].id}
           onClick={function(e) {
             e.preventDefault();
-            this.props.onChangePage();
+            //console.log(Number(e.target.dataset.id))
+            this.props.onChangePage(e.target.dataset.id);
           }.bind(this)}>
             {data[i].title}
           </a>
